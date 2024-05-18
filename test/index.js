6ffc440e336c4e0917c9066b4507e6efe6eb073d
femshima <49227365+femshima@users.noreply.github.com>
2024-05-18 21:35:10 +0900

    make tests able to run

--- test/index.js
+++ test/index.js
@@ -1,17 +1,34 @@
 // @ts-check
 
-/**
- * @fileoverview run following commands before test:
- * ```bash
- * curl -L https://github.com/jpreprocess/jpreprocess/releases/download/v0.8.1/naist-jdic-jpreprocess.tar.gz | tar zx
- * curl -L https://github.com/icn-lab/htsvoice-tohoku-f01/archive/refs/heads/master.zip | tar zx
- * ```
- */
-
-const { it } = require("node:test");
+const { it, describe, before } = require("node:test");
 const { AltJTalk, EncoderType } = require("../lib");
-const path = require("path");
-const assert = require("assert");
+const path = require("node:path");
+const assert = require("node:assert");
+const zlib = require("node:zlib");
+const fs = require("node:fs");
+const { Readable } = require("node:stream");
+const { pipeline } = require("node:stream/promises");
+const tar = require("tar-fs");
+
+/**
+ *
+ * @param {string} url
+ * @param {string} path
+ * @returns {Promise<void>}
+ */
+async function fetchAndExtract(url, path) {
+  const res = await fetch(url);
+  if (!res.ok || !res.body)
+    throw new Error(`Failed to fetch dictionary:\n${await res.text()}`);
+
+  await pipeline([
+    Readable.fromWeb(
+      /** @type {import("node:stream/web").ReadableStream} */ (res.body),
+    ),
+    zlib.createGunzip(),
+    tar.extract(path),
+  ]);
+}
 
 /**
  *
@@ -21,16 +38,12 @@ const assert = require("assert");
  * @returns {Promise<Buffer[]>}
  */
 async function synthesize(altJTalk, inputText, option) {
-  /** @type {Buffer[]} */
-  const frames = [];
-
+  const stream = altJTalk.synthesize(inputText, option);
   return new Promise((resolve, reject) => {
-    altJTalk
-      .synthesize(inputText, option, (err, frame) => {
-        if (err) reject(err);
-        else frames.push(frame);
-      })
-      .then(() => resolve(frames), reject);
+    const bufs = [];
+    stream.on("data", (d) => bufs.push(d));
+    stream.on("error", (err) => reject(err));
+    stream.on("end", () => resolve(bufs));
   });
 }
 
@@ -43,56 +56,73 @@ async function checksum(buffer, checksum) {
   assert.strictEqual(Buffer.from(hash).toString("hex"), checksum);
 }
 
-it("should synthesize identical PCM data", async () => {
-  const pcm = AltJTalk.fromConfig({
-    dictionary: path.join(__dirname, "naist-jdic"),
-    models: [
-      path.join(
+describe("synthesis", () => {
+  before(async () => {
+    if (!fs.existsSync(path.join(__dirname, "naist-jdic"))) {
+      await fetchAndExtract(
+        "https://github.com/jpreprocess/jpreprocess/releases/download/v0.8.1/naist-jdic-jpreprocess.tar.gz",
         __dirname,
-        "htsvoice-tohoku-f01-master",
-        "tohoku-f01-neutral.htsvoice",
-      ),
-    ],
-    encoder: { type: EncoderType.Pcm },
+      );
+    }
+    if (!fs.existsSync(path.join(__dirname, "htsvoice-tohoku-f01-master"))) {
+      await fetchAndExtract(
+        "https://github.com/icn-lab/htsvoice-tohoku-f01/archive/refs/heads/master.tar.gz",
+        __dirname,
+      );
+    }
   });
 
-  const bonsai = await synthesize(pcm, "盆栽", {});
-  await checksum(
-    Buffer.concat(bonsai),
-    "36050aef60896f56bbfe59868a3f57b6bbc5b147",
-  );
+  it("should synthesize identical PCM data", async () => {
+    const pcm = AltJTalk.fromConfig({
+      dictionary: path.join(__dirname, "naist-jdic"),
+      models: [
+        path.join(
+          __dirname,
+          "htsvoice-tohoku-f01-master",
+          "tohoku-f01-neutral.htsvoice",
+        ),
+      ],
+      encoder: { type: EncoderType.Raw },
+    });
 
-  const isBonsai = await synthesize(pcm, "これは，盆栽ですか？", {});
-  await checksum(
-    Buffer.concat(isBonsai),
-    "c0e3f8773a47534862f7e553fb00d48022496b19",
-  );
-});
+    const bonsai = await synthesize(pcm, "盆栽", {});
+    await checksum(
+      Buffer.concat(bonsai),
+      "36050aef60896f56bbfe59868a3f57b6bbc5b147",
+    );
 
-it("should synthesize identical Opus data", async () => {
-  const opus = AltJTalk.fromConfig({
-    dictionary: path.join(__dirname, "naist-jdic"),
-    models: [
-      path.join(
-        __dirname,
-        "htsvoice-tohoku-f01-master",
-        "tohoku-f01-neutral.htsvoice",
-      ),
-    ],
-    encoder: { type: EncoderType.Opus },
+    const isBonsai = await synthesize(pcm, "これは，盆栽ですか？", {});
+    await checksum(
+      Buffer.concat(isBonsai),
+      "c0e3f8773a47534862f7e553fb00d48022496b19",
+    );
   });
 
-  const bonsai = await synthesize(opus, "盆栽", {});
-  assert.strictEqual(bonsai.length, 78);
+  it("should synthesize identical Opus data", async () => {
+    const opus = AltJTalk.fromConfig({
+      dictionary: path.join(__dirname, "naist-jdic"),
+      models: [
+        path.join(
+          __dirname,
+          "htsvoice-tohoku-f01-master",
+          "tohoku-f01-neutral.htsvoice",
+        ),
+      ],
+      encoder: { type: EncoderType.Opus },
+    });
 
-  await checksum(bonsai[25], "26a34471a9a047f9184494b7063699a3b0bc4771");
-  await checksum(bonsai[50], "961959780d81dc24aea2b2c87f6617d12aabf99c");
-  await checksum(bonsai[75], "b805476147d1da6b9c1eb9ede8a7a607d7accfa8");
+    const bonsai = await synthesize(opus, "盆栽", {});
+    assert.strictEqual(bonsai.length, 78);
 
-  const isBonsai = await synthesize(opus, "これは，盆栽ですか？", {});
-  assert.strictEqual(isBonsai.length, 136);
+    await checksum(bonsai[25], "26a34471a9a047f9184494b7063699a3b0bc4771");
+    await checksum(bonsai[50], "961959780d81dc24aea2b2c87f6617d12aabf99c");
+    await checksum(bonsai[75], "b805476147d1da6b9c1eb9ede8a7a607d7accfa8");
 
-  await checksum(isBonsai[40], "9b93326876d0516d808c8bcafbd03221c2f3fbb7");
-  await checksum(isBonsai[80], "10d56cbc58ba71627418a4305e41c454b623711b");
-  await checksum(isBonsai[120], "b805476147d1da6b9c1eb9ede8a7a607d7accfa8");
+    const isBonsai = await synthesize(opus, "これは，盆栽ですか？", {});
+    assert.strictEqual(isBonsai.length, 136);
+
+    await checksum(isBonsai[40], "9b93326876d0516d808c8bcafbd03221c2f3fbb7");
+    await checksum(isBonsai[80], "10d56cbc58ba71627418a4305e41c454b623711b");
+    await checksum(isBonsai[120], "b805476147d1da6b9c1eb9ede8a7a607d7accfa8");
+  });
 });
