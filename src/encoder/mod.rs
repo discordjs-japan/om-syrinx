b43985dec3100c5a3043879a323f82acbd990554
cm-ayf <cm.ayf2734@gmail.com>
2025-03-05 15:32:37 +0900

    introduce SyrinxError

--- src/encoder/mod.rs
+++ src/encoder/mod.rs
@@ -6,14 +6,16 @@ mod pcm;
 
 pub use config::*;
 
+use crate::error::SyrinxResult;
+
 use self::{opus::OpusEncoder, pcm::PcmEncoder};
 
 pub trait Encoder: Send {
-  fn new(condition: &Condition, config: &EncoderConfig) -> napi::Result<Self>
+  fn new(condition: &Condition, config: &EncoderConfig) -> SyrinxResult<Self>
   where
     Self: Sized;
 
-  fn generate(&mut self, generator: &mut SpeechGenerator) -> napi::Result<Vec<u8>>;
+  fn generate(&mut self, generator: &mut SpeechGenerator) -> SyrinxResult<Vec<u8>>;
 }
 
 macro_rules! encoder_new {
@@ -28,11 +30,11 @@ macro_rules! encoder_new {
 }
 
 impl Encoder for Box<dyn Encoder> {
-  fn new(condition: &Condition, config: &EncoderConfig) -> napi::Result<Self> {
+  fn new(condition: &Condition, config: &EncoderConfig) -> SyrinxResult<Self> {
     encoder_new!((condition, config), (Opus, OpusEncoder), (Raw, PcmEncoder))
   }
 
-  fn generate(&mut self, generator: &mut SpeechGenerator) -> napi::Result<Vec<u8>> {
+  fn generate(&mut self, generator: &mut SpeechGenerator) -> SyrinxResult<Vec<u8>> {
     self.as_mut().generate(generator)
   }
 }
--- src/encoder/opus.rs
+++ src/encoder/opus.rs
@@ -1,5 +1,7 @@
 use jbonsai::{engine::Condition, speech::SpeechGenerator};
 
+use crate::error::SyrinxResult;
+
 use super::{pcm::PcmEncoder, Application, Channels, Encoder, EncoderConfig, EncoderType};
 
 pub struct OpusEncoder {
@@ -12,12 +14,11 @@ pub struct OpusEncoder {
 const OPUS_FRAME_SIZE: usize = 20; // ms
 
 impl Encoder for OpusEncoder {
-  fn new(condition: &Condition, config: &EncoderConfig) -> napi::Result<Self>
+  fn new(condition: &Condition, config: &EncoderConfig) -> SyrinxResult<Self>
   where
     Self: Sized,
   {
-    let sample_rate = audiopus::SampleRate::try_from(condition.get_sampling_frequency() as i32)
-      .map_err(|err| napi::Error::new(napi::Status::GenericFailure, err))?;
+    let sample_rate = audiopus::SampleRate::try_from(condition.get_sampling_frequency() as i32)?;
     let channels = config.channels.unwrap_or(Channels::Stereo);
     let mode = config.mode.unwrap_or(Application::Voip);
 
@@ -31,8 +32,7 @@ impl Encoder for OpusEncoder {
     };
 
     let pcm_encoder = PcmEncoder::new(condition, &pcm_config)?;
-    let opus_encoder = audiopus::coder::Encoder::new(sample_rate, channels.into(), mode.into())
-      .map_err(|err| napi::Error::new(napi::Status::GenericFailure, err))?;
+    let opus_encoder = audiopus::coder::Encoder::new(sample_rate, channels.into(), mode.into())?;
 
     Ok(Self {
       output: vec![0; pcm_encoder.speech_len() * channels as usize], // TODO: better capacity estimation
@@ -42,17 +42,14 @@ impl Encoder for OpusEncoder {
     })
   }
 
-  fn generate(&mut self, generator: &mut SpeechGenerator) -> napi::Result<Vec<u8>> {
+  fn generate(&mut self, generator: &mut SpeechGenerator) -> SyrinxResult<Vec<u8>> {
     let mut pcm: Vec<_> = self.pcm_encoder.generate_i16(generator).collect();
     if pcm.is_empty() {
       return Ok(vec![]);
     }
     pcm.resize(self.pcm_encoder.speech_len() * self.channels as usize, 0);
 
-    let size = self
-      .opus_encoder
-      .encode(&pcm, &mut self.output)
-      .map_err(|err| napi::Error::new(napi::Status::GenericFailure, err))?;
+    let size = self.opus_encoder.encode(&pcm, &mut self.output)?;
 
     Ok(self.output[..size].to_vec())
   }
